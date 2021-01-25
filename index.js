978b39bf7ac4b0f96ae3b2d7c8f9462f1faf2a75
David Zirima <davidtzirima@gmail.com>
2021-01-25 18:11:57 +0200

    mongo db connction done

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ import express from 'express'
 import bodyPaser from 'body-parser'
 import mongoose from 'mongoose'
 import  cors from 'cors'
+
+
+
+// initiatal configuration
+const app = express()
+app.use(bodyPaser.json({limit:"300mb",extended:true}))
+app.use(bodyPaser.urlencoded({limit:"300mb",extended:true}))
+app.use(cors())
+
+const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = "mongodb://localhost:27017/memories"; // for local dev
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology :true})
+
+    .then(() => app.listen(PORT,() => console.log(`server running on port : ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify',false);
