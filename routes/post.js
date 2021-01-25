8229ea6229827797956e4ff3fbf5d7a02e2bb4b0
David Zirima <davidtzirima@gmail.com>
2021-01-25 22:47:17 +0200

    hello

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 // all the routing iis going to be handled from here
 import express from 'express'
 
-import getPosts from '../controllers/posts.js'
+import {getPosts} from '../controllers/posts.js'
 
 
 const router = express.Router()
