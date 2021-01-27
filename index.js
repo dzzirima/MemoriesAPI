4ec196193e7d15acfac2a5c6c82ab122a7f41c9b
David Zirima <davidtzirima@gmail.com>
2021-01-28 01:59:26 +0200

    working code ready for deployment

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import bodyPaser from 'body-parser'
 import mongoose from 'mongoose'
 import  cors from 'cors'
+import dotenv from 'dotenv'
 
 
 
 // initiatal configuration
 const app = express()
+dotenv.config()
 app.use(bodyPaser.json({limit:"300mb",extended:true}))
 app.use(bodyPaser.urlencoded({limit:"300mb",extended:true}))
 app.use(cors())
@@ -16,10 +18,10 @@ import postRoutes from './routes/post.js'
 
 app.use('/posts',postRoutes)
 
-const PORT = process.env.PORT|| 5000;
-const CONNECTION_URL = "mongodb://localhost:27017/memories"; // for local dev
+const PORT = process.env.PORT;
+// const CONNECTION_URL = "mongodb://localhost:27017/memories"; // for local dev
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology :true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology :true})
 
     .then(() => app.listen(PORT,() => console.log(`server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message))
