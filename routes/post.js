05d6acf04cf3c25700c9b790257a8f6a02886c34
David Zirima <davidtzirima@gmail.com>
2021-02-18 23:03:18 +0200

    done linking the middleware and routes

--- routes/post.js
+++ routes/post.js
@@ -2,16 +2,18 @@
 import express from 'express'
 
 import {getPosts ,createPosts ,updatePost,deletePost ,likePost} from '../controllers/posts.js'
+// getting the middleware
+import auth from '../middleware/auth.js'
 
 
 const router = express.Router()
 
 
-router.get('/',getPosts)
-router.post('/',createPosts)
-router.patch('/:id',updatePost)
-router.delete('/:id',deletePost)
-router.patch('/:id/likePost',likePost)
+router.get('/',getPosts) // the user can see all the posts even when not loged in
+router.post('/',auth,createPosts) 
+router.patch('/:id',auth,updatePost)
+router.delete('/:id',auth,deletePost) // a user needs to be logged in to  be able to delete ,like and create  a post
+router.patch('/:id/likePost',auth,likePost)
 
 
 export default router
